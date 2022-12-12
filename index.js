a8a50b8cfe44b92fe8cf372ac6eaf470263b5e81
Suyash Mankar <suyashmankar@gmail.com>
2022-12-12 17:46:07 +0530

    minor changes

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ app.set("views", "./views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-console.log(process.env.BASE_URL);
-
-app.use(`/`, require("./routes"));
+app.use("/", require("./routes"));
 
 app.listen(port, function (err) {
   if (err) {
