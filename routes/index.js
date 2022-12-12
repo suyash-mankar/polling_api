45f6b9d387ba0c4306e318fd9c696c2f71af6eb5
Suyash Mankar <suyashmankar@gmail.com>
2022-12-12 16:59:44 +0530

    setup base url for deployment

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get('/', )
+const homeController = require("../controllers/home_controller");
 
+router.get(`${process.env.BASE_URL}/`, homeController.home);
 
-router.use("/questions", require("./questions"));
-router.use("/options", require("./options"));
+router.use(`${process.env.BASE_URL}/questions`, require("./questions"));
+router.use(`${process.env.BASE_URL}/options`, require("./options"));
 
 module.exports = router;
